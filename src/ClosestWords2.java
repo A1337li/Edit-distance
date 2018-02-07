35844a874fd4c961352207268ac3aeb127541c48
mahdibasketball <35520563+mahdibasketball@users.noreply.github.com>
2018-02-07 19:12:40 +0100

    updates...

--- src/ClosestWords2.java
+++ src/ClosestWords2.java
@@ -9,48 +9,24 @@ public class ClosestWords2 {
 
     int closestDistance = -1;
 
-    int[][] oldMatrix = new int[30][30];
-    String oldWord = " ";
-
-    public int compareWords(String word1, String word2) {
-        int x = 0;
-        int y = Math.min(word1.length(), word2.length());
-        while (x < y && y > 0){
-            if (word1.charAt(x) == word2.charAt(x)) {
-                x++;
-            }
-            else{
-                break;
-            }
-
-        }
-        return x;
-    }
-
-    int getmin(int x, int y, int z) {
-        return Math.min(x, Math.min(y,z));
-    }
+    int[][] oldMatrix = new int[40][40];
+    String oldWord = "q";
 
     int[][] partDist(String w1, String w2, int w1len, int w2len) {
         int compareVal = compareWords(oldWord,w2);
 
         for (int i = 0; i <= w1len; i++) {
-
             for (int j = compareVal; j <= w2len; j++) {
-                if (i == 0)
-                    oldMatrix[i][j] = j;
-
-                else if (j == 0)
-                    oldMatrix[i][j] = i;
-                else if (w1.charAt(i - 1) == w2.charAt(j - 1))
+                if (i == 0 || j == 0)
+                    oldMatrix[i][j] = i+j;
+                else if (w1.charAt(i - 1) == w2.charAt(j - 1)) {
                     oldMatrix[i][j] = oldMatrix[i - 1][j - 1];
-                else
+                }
+                else {
                     oldMatrix[i][j] = 1 + getmin(oldMatrix[i][j - 1],
                             oldMatrix[i - 1][j],
                             oldMatrix[i - 1][j - 1]);
-
-
-
+                }
             }
         }
 
@@ -63,8 +39,11 @@ public class ClosestWords2 {
     }
 
     public ClosestWords2(String w, List<String> wordList) {
-        //oldMatrix = new int[10][10];
         for (String s : wordList) {
+            if (closestDistance != -1 && Math.abs(s.length()-w.length()) > closestDistance){
+                continue;
+            }
+
             int dist = Distance(w, s);
             oldWord = s;
             // System.out.println("d(" + w + "," + s + ")=" + dist);
@@ -85,4 +64,18 @@ public class ClosestWords2 {
     List<String> getClosestWords() {
         return closestWords;
     }
+
+    public int compareWords(String word1, String word2) {
+        int x = 0;
+        int y = Math.min(word1.length(), word2.length());
+        while (x < y && word1.charAt(x) == word2.charAt(x)){
+            x++;
+        }
+        return x;
+    }
+
+    int getmin(int x, int y, int z) {
+        return Math.min(x, Math.min(y,z));
+    }
+
 }
